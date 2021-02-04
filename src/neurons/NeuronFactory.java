8e899f8df352057bc62cb258dafd1d04bba49232
Stelios Zappidis <Stelioszapp@gmail.com>
2021-02-04 12:14:50 +0200

    Update NeuronFactory.java

--- src/neurons/NeuronFactory.java
+++ src/neurons/NeuronFactory.java
@@ -7,13 +7,11 @@ public class NeuronFactory{
 	
 	public ArrayList<Neuron> getNuerons(ArrayList<double[]> examplesList, int M){
 		ArrayList<Neuron> neurons = new ArrayList<Neuron>();
-		int position; //The number of the random example we will use for our neurons
 		Random r = new Random();
 		int range = examplesList.size(); //the range of random number it can get
 		
 		for(int i=0; i<M; i++){
-			position = r.nextInt(range); //get a random line from our text with examples
-			double [] l = examplesList.get(position);
+			double [] l = examplesList.get(r.nextInt(range));
 			
 			double x = l[0];
 			double y = l[1];
