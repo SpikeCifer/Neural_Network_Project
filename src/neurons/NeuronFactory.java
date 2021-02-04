84547efebad665a98ea67a2b27ba0091a6564c26
Stelios Zappidis <Stelioszapp@gmail.com>
2021-02-04 12:20:41 +0200

    Update NeuronFactory.java

--- src/neurons/NeuronFactory.java
+++ src/neurons/NeuronFactory.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class NeuronFactory{
+	private static X_DIMENSION = 0;
+	private static Y_DIMENSION = 1;
 	
 	public ArrayList<Neuron> getNuerons(ArrayList<double[]> examplesList, int M){
 		ArrayList<Neuron> neurons = new ArrayList<Neuron>();
@@ -11,8 +13,8 @@ public class NeuronFactory{
 		int range = examplesList.size(); //the range of random number it can get
 		
 		for(int i = 0; i < M; i++){
-			double [] l = examplesList.get(r.nextInt(range));			
-			Neuron n = new Neuron(l[0],l[1]);
+			double [] example = examplesList.get(r.nextInt(range));			
+			Neuron n = new Neuron(example[X_DIMENSION],example[Y_DIMENSION]);
 			neurons.add(n);
 		}
 		
